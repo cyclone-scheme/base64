724b0c572a4a4f7ed83f0c1991a0693787c472a6
Justin Ethier <justin.ethier@gmail.com>
2020-03-23 22:25:01 -0400

    Relocating files

---
+++ package.scm
@@ -0,0 +1,14 @@
+(package
+ (name           base64)
+ (version        0.1)
+ (license        "BSD")
+ (authors        "Alex Shinn")
+ (maintainers    "Justin Ethier <justin.ethier at gmail dot com>")
+ (description    "base64 encoding/decoding library")
+ (tags           "networking")
+ (docs           "https://github.com/cyclone-scheme/base64")
+ (test           "test.scm")
+
+ (library
+     (name (cyclone base64))
+   (description "Base64 encoding/decoding library")))
